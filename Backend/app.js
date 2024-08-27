00f7034949a585df90745d69f82e5f0a9552affd
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-27 18:40:38 +0530

    Added middlewares

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
 import cors from "cors"
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
 const app = express();
+
+
 config({path: "./config/config.env"})
 
 app.use(cors(
@@ -10,5 +15,14 @@ app.use(cors(
     credentials:true
     }
 ))
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir: '/tmp/'
+}))
 
 export default app;
