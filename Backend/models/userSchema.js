d2f0e121dc707c5dfcbe824b665b96475aa989b3
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-28 18:06:11 +0530

    user schema created

---
+++ Backend/models/userSchema.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose"
+import validator from "validator"
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        minLength:[3, "First Name must contain atleast 3 characters!"]
+    },
+    lastName:{
+        type: String,
+        required: true,
+        minLength:[3, "Last Name must contain atleast 3 characters!"]
+    },
+    email:{
+        type: String,
+        required: true,
+        validate : [validator.isEmail, "Please provide a valid Email!"]
+    },
+    phone:{
+        type: String,
+        required: true,
+        maxLength:[10, "Phone number must contain exact 10 digits!"],
+        minLength:[10, "Phone number must contain exact 10 digits!"]
+    },
+    aadhar:{
+        type: String,
+        required: true,
+        maxLength:[12, "Aadhar must contain exact 12 digits!"],
+        minLength:[12, "Aadhar must contain exact 12 digits!"]
+    },
+    dob:{
+        type: Date,
+        required: [true, "Date of Birth is required!"]
+    },
+    gender:{
+        type: String,
+        required : true,
+        enum: ["Male", "Female", "Others"]
+    },
+    password:{
+        type: String,
+        required:true,
+        minLength: [8, "Password must contain atleat 8 characters!"],
+        select: false
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["Admin", "Patient", "Doctor"]
+    },
+    doctrDptmnt:{
+        type: String
+    },
+    doctrAvatar:{
+        public_id: String,
+        url: String
+    }
+})
+
+
+
+
+export const User = mongoose.model("Message",userSchema)
