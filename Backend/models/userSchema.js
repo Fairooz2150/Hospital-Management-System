c917fdb7044d358c8a040222ff52ddc176ff1bcc
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-28 21:25:38 +0530

    Created a jwt in userSchema, token for user

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import validator from "validator"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -70,5 +71,10 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword,this.password)
 }
 
+userSchema.methods.generateJsonWebToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY, {
+         expiresIn: process.env.JWT_EXPIRES,
+        })
+}
 
 export const User = mongoose.model("User",userSchema)
