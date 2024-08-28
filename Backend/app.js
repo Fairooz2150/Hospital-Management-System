8fa87beb36a6ce8942410e8d27359b22614e1fb8
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-28 17:25:49 +0530

    Created error middleware and handle errors

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
 import messageRouter from "./router/messageRouter.js"
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
@@ -30,5 +31,7 @@ app.use(fileUpload({
 app.use("/api/v1/message", messageRouter)
 
 dbConnection();
+
+app.use(errorMiddleware)
 
 export default app;
--- Backend/controller/messageController.js
+++ Backend/controller/messageController.js
@@ -1,14 +1,13 @@
 import { Message } from "../models/messageSchema.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+
 
 export const sendMessage = catchAsyncErrors(async (req, res, next) => {
     const { firstName, lastName, email, phone, message } = req.body;
 
     if (!firstName || !lastName || !email || !phone || !message) {
-        return res.status(400).json({
-            success: false,
-            message: "Please complete the full form."
-        })
+        return next(new ErrorHandler("Please Fill Full Form", 400))
     }
 
     await Message.create({ firstName, lastName, email, phone, message })
