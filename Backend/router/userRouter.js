a6881b07443b31109abc149900a8164102f94ba5
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-29 02:12:32 +0530

    Checks authentiactions for user in router

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { addNewAdmin, login, patientRegister } from "../controller/userController.js";
+import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/patient/register", patientRegister)
+router.post("/patient/register", isPatientAuthenticated, patientRegister)
 router.post("/login", login)
-router.post("/admin/addnew", addNewAdmin)
+router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin)
 
 
 
