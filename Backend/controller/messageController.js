2d56f47b849e95b8fd76f2f71517df213b1768bf
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-28 12:12:29 +0530

    catchAsyncError middleware created

--- Backend/controller/messageController.js
+++ Backend/controller/messageController.js
@@ -1,19 +1,19 @@
 import { Message } from "../models/messageSchema.js";
 
-export const sendMessage = async (req, res, next) => {
-    const { firstName, lastName, email, phone, message } = req.body;
+export const sendMessage = async (req,res,next) => {
+    const {firstName, lastName, email, phone, message} = req.body;
 
-    if (!firstName || !lastName || !email || !phone || !message) {
-        return res.status(400).json({
-            success: false,
-            message: "Please complete the full form."
-        })
-    }
+   if(!firstName || !lastName || !email || !phone || !message) {
+    return res.status(400).json({
+        success: false,
+        message: "Please complete the full form."
+    })
+ }
 
-    await Message.create({ firstName, lastName, email, phone, message })
+    await Message.create({firstName, lastName, email, phone, message})
     res.status(200).json({
         success: true,
         message: "Message Send Successfully!"
-    })
-
+     })
+   
 }
