ea37c378940bc77b489c6344e7f0ba422fe221fa
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-28 18:17:36 +0530

    compare passwords by entered password and stored password

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -66,6 +66,9 @@ userSchema.pre("save", async function(next){
     this.password = await bcrypt.hash(this.password,10)
 })
 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password)
+}
 
 
 export const User = mongoose.model("User",userSchema)
