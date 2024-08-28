eb9dc50635126cc82ad3db45865e96c5a18c40de
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-29 00:58:28 +0530

    Login function created

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -20,3 +20,34 @@ export const patientRegister = catchAsyncErrors(async(req,res,next)=>{
         message: "User Registered!"
     })
 })
+
+
+export const login = catchAsyncErrors(async(req,res,next)=>{
+    const {email, password, confirmPassword, role } = req.body;
+    if(!email || !password || !confirmPassword || !role ){
+        return next(new ErrorHandler("Please provide all details!", 400))
+    }
+    if(password !== confirmPassword){
+        return next(new ErrorHandler("Password and Confirm Password Do not Match!", 400))
+    }
+
+    const user = await User.findOne({email}).select("+password");
+    
+    if(!user){
+        return next(new ErrorHandler("Invalid Password or Email!", 400))
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+    
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid Password or Email!", 400))
+    }
+
+    if(role !== user.role){
+        return next(new ErrorHandler("User with this role not found!", 400))
+    }
+    res.status(200).json({
+        success:true,
+        message: "User Logged In Successfully!"
+    })
+})
