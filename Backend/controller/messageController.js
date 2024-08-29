436210dddba1561ebc9e1d7b79d5e7ee5c218bcc
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-29 16:21:32 +0530

    Get all message by admin

--- Backend/controller/messageController.js
+++ Backend/controller/messageController.js
@@ -15,5 +15,14 @@ export const sendMessage = catchAsyncErrors(async (req, res, next) => {
         success: true,
         message: "Message Send Successfully!"
     })
+})
+
+
+export const getAllMessages = catchAsyncErrors(async(req,res,next)=>{
+    const messages = await Message.find();
+    res.status(200).json({
+        success: true,
+        messages
+    })
 
 })
--- Backend/router/messageRouter.js
+++ Backend/router/messageRouter.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { sendMessage } from "../controller/messageController.js";
+import { getAllMessages, sendMessage } from "../controller/messageController.js";
+import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage)
+router.get("/getall", isAdminAuthenticated, getAllMessages)
 
 export default router
