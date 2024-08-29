ad74e41db70a572d3d2abfbbdf868540a08e63ba
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-29 18:24:38 +0530

    Appointment delete by admin

--- Backend/controller/appointmentController.js
+++ Backend/controller/appointmentController.js
@@ -91,7 +91,8 @@ export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
 
 export const updateAppointmentStatus = catchAsyncErrors(async (req,res,next)=>{
     const {id} = req.params;
-    let appointment = await Appointment.findById(id)
+ 
+       let appointment = await Appointment.findById(id)
     if(!appointment){
        return next(new ErrorHandler("Appointment not found!", 404)) 
     }
@@ -104,5 +105,20 @@ export const updateAppointmentStatus = catchAsyncErrors(async (req,res,next)=>{
         success: true,
         message: "Appointment Status Updated!",
         appointment
+    })
+})
+
+
+export const deleteAppointment = catchAsyncErrors(async(req,res,next)=>{
+    const {id} = req.params;
+       let appointment = await Appointment.findById(id)
+       if(!appointment){
+        return next(new ErrorHandler("Appointment not found!", 404)) 
+     }
+
+     await appointment.deleteOne();
+     res.status(200).json({
+        success: true,
+        message: "Appointment Deleted!",
     })
 })
--- Backend/router/appointmentRouter.js
+++ Backend/router/appointmentRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
+import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
 import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/post",isPatientAuthenticated, postAppointment)
 router.get("/getall",isAdminAuthenticated, getAllAppointments)
 router.put("/update/:id",isAdminAuthenticated, updateAppointmentStatus)
+router.delete("/delete/:id",isAdminAuthenticated, deleteAppointment)
 
 export default router;
