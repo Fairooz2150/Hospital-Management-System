b049a3ce3f9b376a7c5ed9593199ee69d5a583af
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-29 17:54:36 +0530

    Get all aappointments by admin

--- Backend/controller/appointmentController.js
+++ Backend/controller/appointmentController.js
@@ -1,9 +1,9 @@
-import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js"
 import ErrorHandler from "../middlewares/errorMiddleware.js"
-import {Appointment} from "../models/appointmentSchema.js"
-import {User} from "../models/userSchema.js"
+import { Appointment } from "../models/appointmentSchema.js"
+import { User } from "../models/userSchema.js"
 
-export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
+export const postAppointment = catchAsyncErrors(async (req, res, next) => {
     const {
         firstName,
         lastName,
@@ -20,40 +20,39 @@ export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
         address
     } = req.body
 
-    if(!firstName ||
-       ! lastName ||
-       ! email ||
-       ! phone ||
-       ! aadhar ||
-       ! dob ||
-       ! gender ||
-       ! appointment_date ||
-       ! department ||
-       ! doctor_firstName ||
-       ! address)
-       {
-        return next (new ErrorHandler("Please Fill Full Form!", 400))
-       }
+    if (!firstName ||
+        !lastName ||
+        !email ||
+        !phone ||
+        !aadhar ||
+        !dob ||
+        !gender ||
+        !appointment_date ||
+        !department ||
+        !doctor_firstName ||
+        !address) {
+        return next(new ErrorHandler("Please Fill Full Form!", 400))
+    }
 
-       const isConflict = await User.find({
+    const isConflict = await User.find({
         firstName: doctor_firstName,
         lastName: doctor_lastName,
         role: "Doctor",
         doctrDptmnt: department
-       })
+    })
 
-       if(isConflict.length === 0){
-        return next (new ErrorHandler("Doctor not found!", 404))
-       }
+    if (isConflict.length === 0) {
+        return next(new ErrorHandler("Doctor not found!", 404))
+    }
 
-       if(isConflict.length > 1){
-        return next (new ErrorHandler("Doctors conflict! Please contact through email or phone.", 404))
-       }
+    if (isConflict.length > 1) {
+        return next(new ErrorHandler("Doctors conflict! Please contact through email or phone.", 404))
+    }
 
-       const doctorId = isConflict[0]._id;
-       const patientId = req.user._id;
+    const doctorId = isConflict[0]._id;
+    const patientId = req.user._id;
 
-       const appointment = await Appointment.create({
+    const appointment = await Appointment.create({
         firstName,
         lastName,
         email,
@@ -63,7 +62,7 @@ export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
         gender,
         appointment_date,
         department,
-        doctor:{
+        doctor: {
             firstName: doctor_firstName,
             lastName: doctor_lastName
         },
@@ -71,10 +70,20 @@ export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
         address,
         doctorId,
         patientId
-       })
-       res.status(200).json({
+    })
+    res.status(200).json({
         success: true,
-        message: "Appointment Sent Successfully!"
-       })
+        message: "Appointment Sent Successfully!",
+        appointment
+    })
 
+})
+
+
+export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
+    const appointments = await Appointment.find();
+    res.status(200).json({
+        success: true,
+        appointments
+    })
 })
--- Backend/router/appointmentRouter.js
+++ Backend/router/appointmentRouter.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { postAppointment } from "../controller/appointmentController.js";
-import { isPatientAuthenticated } from "../middlewares/auth.js";
+import { getAllAppointments, postAppointment } from "../controller/appointmentController.js";
+import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 
 router.post("/post",isPatientAuthenticated, postAppointment)
+router.get("/getall",isAdminAuthenticated, getAllAppointments)
 
 export default router;
