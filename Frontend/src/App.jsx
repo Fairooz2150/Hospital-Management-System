2eeb850adeeb476f651b5d191b11980a889129fb
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-30 18:46:11 +0530

    import and use ToastContainer on App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AboutUs from './pages/AboutUs';
+import Appointment from './pages/Appointment';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
     <Router>
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
+        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </Router>
       
     </>
