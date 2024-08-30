a5a1ea9f6aa6a795ab62a1e18e242a1b52e6d5c8
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-30 22:31:45 +0530

    Modified p tag contents in biography

--- Frontend/src/components/Biography.jsx
+++ Frontend/src/components/Biography.jsx
@@ -10,19 +10,12 @@ const Biography = ({ imageUrl }) => {
             <div className="banner">
                 <p>Biography</p>
                 <h3>Who We Are</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui harum commodi, sunt pariatur
-                    corporis molestias voluptatum facilis quidem libero doloremque. Ipsum explicabo ratione magni
-                    laboriosam minus necessitatibus dolor repellat iste nemo animi quia blanditiis deserunt rerum
-                    error non voluptatem rem sunt obcaecati perspiciatis, doloremque ab dignissimos aperiam consectetur
-                    consequuntur! Harum.
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, reprehenderit sapiente facilis, aspernatur, vero facere blanditiis inventore veniam dolores fugiat deleniti! Repellendus quibusdam ipsa fugit qui id ab dolor cumque, repudiandae totam, nesciunt autem nihil?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis blanditiis consequatur corporis.</p>
-                <p>Lorem, ipsum dolor.</p>
+                <p>Life Care Hospital is a premier healthcare provider dedicated to offering comprehensive medical services with exceptional care. Our expert team of healthcare professionals is committed to delivering personalized treatment, ensuring that every patient receives the highest standard of care.</p>
+                <p>Our facility is equipped with state-of-the-art technology and staffed by highly skilled professionals, all focused on delivering excellent care.</p>
+                <p>Our team of doctors, nurses, and support staff work collaboratively to provide comprehensive medical solutions.</p>
+                <p> We believe in a patient-centered approach, where each individual's needs are addressed with attention and expertise.</p>
+                <p>We are dedicated to making a positive impact on the lives of our patients, providing the care and support they need to achieve their best health.</p>
             </div>
-
         </div>
     );
 }
