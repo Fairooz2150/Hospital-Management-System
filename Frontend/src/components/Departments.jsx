073383d687e97ecc457e6e3093d9c2381a42e0c1
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-30 22:44:50 +0530

    import Carousel and pass defined object of data  for  responsive carousel

--- Frontend/src/components/Departments.jsx
+++ Frontend/src/components/Departments.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Departments = () => {
+  
+  const responsive = {
+    extraLarge: {
+      breakpoint: { max: 3000, min: 1324 },
+      items: 4,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    large: {
+      breakpoint: { max: 1324, min: 1005 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    medium: {
+      breakpoint: { max: 1005, min: 700 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    small: {
+      breakpoint: { max: 700, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  }
+
   return (
-    <div>
-      
+    <div className='container departments'>
+      <h2>Departments</h2>
+      <Carousel responsive={responsive}>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+
     </div>
   );
 }
