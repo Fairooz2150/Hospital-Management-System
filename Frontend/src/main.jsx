cdd52d01f7b2792c642733a19047a82f0a8cbbac
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-30 18:23:29 +0530

    Setup contxt and useState for user authenticated or not in  Main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { useState } from 'react';
+
+export const Context = createContext({isAuthenticated: false});
+
+const AppWrapper = ()=>{
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(false);
+
+  return(
+    <Context.Provider value={{isAuthenticated, setIsAuthenticated, user, setUser }}>
+        <App />
+    </Context.Provider>
+  )
+
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <AppWrapper />
   </StrictMode>,
 )
