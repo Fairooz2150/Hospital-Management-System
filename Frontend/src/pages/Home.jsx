76ff06fda917d9990aa42482949cb0988c8388a9
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-30 20:38:22 +0530

    Create Biography,deptmnt,hero,msgform components

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Biography from '../components/Biography';
+import Departments from '../components/Departments';
+import MessageForm from '../components/MessageForm';
 
 const Home = () => {
   return (
     <>
-      
+      <Hero/>
+      <Biography/>
+      <Departments/>
+      <MessageForm/>
+
     </>
   );
 }
