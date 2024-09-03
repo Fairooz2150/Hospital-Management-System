10083277e8dab67fad7a23f48191bafe5fd1664f
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 03:11:09 +0530

    Add New Admin page designed

--- Frontend-Admin/src/components/AddNewAdmin.jsx
+++ Frontend-Admin/src/components/AddNewAdmin.jsx
@@ -1,11 +1,134 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const AddNewAdmin = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [dob, setDob] = useState("");
+  const [aadhar, setAadhar] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const addNewAdmin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/user/admin/addnew",
+        {
+          firstName,
+          lastName,
+          email,
+          phone,
+          password,
+          gender,
+          aadhar,
+          dob,
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      toast.success(response.data.message);
+      setIsAuthenticated(true);
+      navigateTo("/");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="page">
+        <div className="container form-component add-admin-form">
+          <img src="/logo.png" alt="Life Care" className="logo" />
+          <h1 className="form-title">Add New Admin</h1>
+
+          <form onSubmit={addNewAdmin}>
+            <div>
+              <input
+                type="text"
+                value={firstName}
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <input
+                type="text"
+                value={lastName}
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <input
+                type="email"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="number"
+                value={phone}
+                placeholder="Phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <input
+                type="number"
+                value={aadhar}
+                placeholder="Aadhar Number"
+                onChange={(e) => setAadhar(e.target.value)}
+              />
+              <input
+                type="date"
+                value={dob}
+                placeholder="Date of Birth"
+                onChange={(e) => setDob(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <select
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Others">Others</option>
+              </select>
+
+              <input
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+     
+            <div style={{ justifyContent: "center", alignItems: "center" }}>
+              <button type="submit">Add New Admin</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default AddNewAdmin;
