6dea604802a6d7b64aa05c8ae0ac9a03db173451
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:16:19 +0530

    fetch user and check authenticated

--- Frontend-Admin/src/App.jsx
+++ Frontend-Admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Messages from "./components/Messages";
@@ -9,8 +9,29 @@ import Doctors from "./components/Doctors";
 import Sidebar from "./components/Sidebar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./main";
+import axios from "axios";
 
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/admin/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
   return (
     <>
       <Router>
