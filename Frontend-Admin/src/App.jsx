88793f7e761b204f72c61b5ce91b39fc554c44c9
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-03 23:58:27 +0530

    router implemented in App.jsx

--- Frontend-Admin/src/App.jsx
+++ Frontend-Admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes, route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
