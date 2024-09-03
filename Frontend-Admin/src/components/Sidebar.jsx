c0e9ed4201004121834425fd7be5ff4bf8c21104
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:59:23 +0530

    Handle logout function on click

--- Frontend-Admin/src/components/Sidebar.jsx
+++ Frontend-Admin/src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ import { FaUserDoctor } from "react-icons/fa6";
 import { MdAddModerator } from "react-icons/md";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
@@ -16,47 +18,64 @@ const Sidebar = () => {
 
   const navigateTo = useNavigate();
 
-  const gotoHome = ()=>{
-    navigateTo("/")
-    setShow(!show)
-  }
-  
-  const gotoDoctors = ()=>{
-    navigateTo("/doctors")
-    setShow(!show)
-  }
+  const gotoHome = () => {
+    navigateTo("/");
+    setShow(!show);
+  };
 
+  const gotoDoctors = () => {
+    navigateTo("/doctors");
+    setShow(!show);
+  };
 
-  const gotoMessages = ()=>{
-    navigateTo("/messages")
-    setShow(!show)
-  }
+  const gotoMessages = () => {
+    navigateTo("/messages");
+    setShow(!show);
+  };
 
-  const gotoAddNewDoctor = ()=>{
-    navigateTo("/doctor/addnew")
-    setShow(!show)
-  }
+  const gotoAddNewDoctor = () => {
+    navigateTo("/doctor/addnew");
+    setShow(!show);
+  };
 
-  const gotoAddNewAdmin = ()=>{
-    navigateTo("/admin/addnew")
-    setShow(!show)
-  }
+  const gotoAddNewAdmin = () => {
+    navigateTo("/admin/addnew");
+    setShow(!show);
+  };
 
+  const handleLogout = async () => {
+    await axios
+      .get("http://localhost:4000/api/v1/user/admin/logout", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        setIsAuthenticated(false);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
 
   return (
-    <nav
-      style={!isAuthenticated ? { display: "none" } : { display: "flex" }}
-      className={show ? "show sidebar" : "sidebar"}
-    >
-      <div className="links">
-        <TiHome onClick={gotoHome}/>
-        <FaUserDoctor onClick={gotoDoctors}/>
-        <MdAddModerator onClick={gotoAddNewAdmin}/>
-        <IoPersonAddSharp onClick={gotoAddNewDoctor}/>
-        <AiFillMessage onClick={gotoMessages}/>
-        <RiLogoutBoxFill />
+    <>
+      <nav
+        style={!isAuthenticated ? { display: "none" } : { display: "flex" }}
+        className={show ? "show sidebar" : "sidebar"}
+      >
+        <div className="links">
+          <TiHome onClick={gotoHome} />
+          <FaUserDoctor onClick={gotoDoctors} />
+          <MdAddModerator onClick={gotoAddNewAdmin} />
+          <IoPersonAddSharp onClick={gotoAddNewDoctor} />
+          <AiFillMessage onClick={gotoMessages} />
+          <RiLogoutBoxFill onClick={handleLogout} />
+        </div>
+      </nav>
+      <div className="wrapper">
+        
       </div>
-    </nav>
+    </>
   );
 };
 
