101750e9c3913797a6e050fe18e3a4a6adbe0740
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 01:37:21 +0530

    Login page designed

--- Frontend-Admin/src/components/Login.jsx
+++ Frontend-Admin/src/components/Login.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const navigateTo = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/user/login",
+        { email, password, confirmPassword, role: "Admin" },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      toast.success(response.data.message);
+      setIsAuthenticated(true);
+      navigateTo("/");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
-    <div>
-      
+    <>
+       <div className="container form-component ">
+     <img src="/logo.png" alt="Life Care" className="logo"/>
+     <h1 className="form-title">Welcome to Life Care</h1>
+     <p>Only Admins are allowed to access these resources!</p>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setconfirmPassword(e.target.value)}
+          placeholder="Confirm Password"
+        />
+
+       
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
+    </>
   );
 }
 
