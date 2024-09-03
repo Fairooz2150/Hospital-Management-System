725f0fc774e0a71f1384bcc124332623447041f3
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:08:24 +0530

    add sidebar

--- Frontend-Admin/src/App.jsx
+++ Frontend-Admin/src/App.jsx
@@ -6,18 +6,20 @@ import AddNewDoctor from "./components/AddNewDoctor";
 import AddNewAdmin from "./components/AddNewAdmin";
 import Login from "./components/Login";
 import Doctors from "./components/Doctors";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
       <Router>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/doctor/addnew" element={<AddNewDoctor />} />
           <Route path="/admin/addnew" element={<AddNewAdmin />} />
-          <Route path='/messages' element={<Messages/>} />
-          <Route path='/doctors' element={<Doctors/>} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/doctors" element={<Doctors />} />
         </Routes>
       </Router>
     </>
