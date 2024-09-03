1c334e2926a0e4aa36a6894b946f77b4722c2504
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:47:09 +0530

    Icons added in sidebar nav

--- Frontend-Admin/src/components/Sidebar.jsx
+++ Frontend-Admin/src/components/Sidebar.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { TiHome } from "react-icons/ti";
+import { RiLogoutBoxFill } from "react-icons/ri";
+import { AiFillMessage } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUserDoctor } from "react-icons/fa6";
+import { MdAddModerator } from "react-icons/md";
+import { IoPersonAddSharp } from "react-icons/io5";
 
 const Sidebar = () => {
+  const [show, setShow] = useState(false);
+
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
   return (
-    <div>
-      
-    </div>
+    <nav
+      style={!isAuthenticated ? { display: "none" } : { display: "flex" }}
+      className={show ? "show sidebar" : "sidebar"}
+    >
+      <div className="links">
+        <TiHome />
+        <FaUserDoctor />
+        <MdAddModerator />
+        <IoPersonAddSharp />
+        <AiFillMessage />
+        <RiLogoutBoxFill />
+      </div>
+    </nav>
   );
-}
+};
 
 export default Sidebar;
