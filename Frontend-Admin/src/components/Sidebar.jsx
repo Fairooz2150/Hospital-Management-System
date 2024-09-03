9c1ad5c88948bd838b6d2bb2f95491f85034c9b4
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:55:05 +0530

    Add goto functions onClick

--- Frontend-Admin/src/components/Sidebar.jsx
+++ Frontend-Admin/src/components/Sidebar.jsx
@@ -7,11 +7,41 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUserDoctor } from "react-icons/fa6";
 import { MdAddModerator } from "react-icons/md";
 import { IoPersonAddSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
 
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
+  const navigateTo = useNavigate();
+
+  const gotoHome = ()=>{
+    navigateTo("/")
+    setShow(!show)
+  }
+  
+  const gotoDoctors = ()=>{
+    navigateTo("/doctors")
+    setShow(!show)
+  }
+
+
+  const gotoMessages = ()=>{
+    navigateTo("/messages")
+    setShow(!show)
+  }
+
+  const gotoAddNewDoctor = ()=>{
+    navigateTo("/doctor/addnew")
+    setShow(!show)
+  }
+
+  const gotoAddNewAdmin = ()=>{
+    navigateTo("/admin/addnew")
+    setShow(!show)
+  }
+
 
   return (
     <nav
@@ -19,11 +49,11 @@ const Sidebar = () => {
       className={show ? "show sidebar" : "sidebar"}
     >
       <div className="links">
-        <TiHome />
-        <FaUserDoctor />
-        <MdAddModerator />
-        <IoPersonAddSharp />
-        <AiFillMessage />
+        <TiHome onClick={gotoHome}/>
+        <FaUserDoctor onClick={gotoDoctors}/>
+        <MdAddModerator onClick={gotoAddNewAdmin}/>
+        <IoPersonAddSharp onClick={gotoAddNewDoctor}/>
+        <AiFillMessage onClick={gotoMessages}/>
         <RiLogoutBoxFill />
       </div>
     </nav>
