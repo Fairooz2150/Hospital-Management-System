b235984bce7862a461cad9a32f571bc05b63cb32
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-03 23:55:53 +0530

    User Authenticated useStates provided through context provider by  appwrapper

--- Frontend-Admin/src/main.jsx
+++ Frontend-Admin/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createContext, StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
+export const Context = createContext({ isAuthenticated: false });
 
-createRoot(document.getElementById('root')).render(
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(false);
+
+  return (
+    <Context.Provider
+      value={{ isAuthenticated, setIsAuthenticated, user, setUser }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppWrapper />
+  </StrictMode>
+);
