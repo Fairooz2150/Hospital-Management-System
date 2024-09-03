2d2f5ee59e98fa9e413ee5f2191401db68b42119
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-04 00:07:22 +0530

    Routers created

--- Frontend-Admin/src/App.jsx
+++ Frontend-Admin/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Messages from "./components/Messages";
+import AddNewDoctor from "./components/AddNewDoctor";
+import AddNewAdmin from "./components/AddNewAdmin";
+import Login from "./components/Login";
+import Doctors from "./components/Doctors";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route/>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/doctor/addnew" element={<AddNewDoctor />} />
+          <Route path="/admin/addnew" element={<AddNewAdmin />} />
+          <Route path='/messages' element={<Messages/>} />
+          <Route path='/doctors' element={<Doctors/>} />
         </Routes>
       </Router>
     </>
   );
-}
+};
 
 export default App;
