0bea716391ecaabf13e939f9d591236c9141edbe
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-06 01:33:28 +0530

    DashBoard Design completed

--- Frontend-Admin/src/components/Dashboard.jsx
+++ Frontend-Admin/src/components/Dashboard.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../main";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
+import { GoCheckCircleFill } from "react-icons/go";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
   const { isAuthenticated, user } = useContext(Context);
@@ -17,12 +20,32 @@ const Dashboard = () => {
         );
         setAppointments(data.appointments);
       } catch (error) {
-        setAppointments({});
+        setAppointments([]);
         console.log("Some Error Occured While Fetching Appointments", error);
       }
     };
     fetchAppointments();
   }, []);
+
+  const updateStatus = async (appointmentId, status) => {
+    try {
+      const { data } = await axios.put(
+        `http://localhost:4000/api/v1/appointment/update/${appointmentId}`,
+        { status },
+        { withCredentials: true }
+      );
+      setAppointments((prevAppointments) => 
+        prevAppointments.map((appointment) => 
+          appointment._id === appointmentId
+            ? { ...appointment, status }
+            : appointment
+        )
+      );
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
 
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />;
@@ -62,6 +85,70 @@ const Dashboard = () => {
               <li>ENT</li>
             </ul>
           </div>
+        </div>
+
+        <div className="banner">
+          <h5>Appointments</h5>
+          <table>
+            <thead>
+              <tr>
+                <th>Patient</th>
+                <th>Date</th>
+                <th>Doctor</th>
+                <th>Department</th>
+                <th>Status</th>
+                <th>Visited</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appointments && appointments.length > 0 ? (
+                appointments.map((appointment) => {
+                  return (
+                    <tr key={appointment._id}>
+                      <td>{`${appointment.firstName} ${appointment.lastName}`}</td>
+                      <td>{appointment.appointment_date}</td>
+                      <td>{`${appointment.doctor.firstName} ${appointment.doctor.lastName}`}</td>
+                      <td>{appointment.department}</td>
+                      <td>
+                        <select
+                          className={
+                            appointment.status === "Pending"
+                              ? "value-pending"
+                              : appointment.status === "Rejected"
+                              ? "value-rejected"
+                              : "value-accepted"
+                          }
+                          value={appointment.status}
+                          onChange={(e) =>
+                            updateStatus(appointment._id, e.target.value)
+                          }
+                        >
+                          <option className="value-pending" value="Pending">
+                            Pending
+                          </option>
+                          <option className="value-accepted" value="Accepted">
+                            Accepted
+                          </option>
+                          <option className="value-accepted" value="Rejected">
+                            Rejected
+                          </option>
+                        </select>
+                      </td>
+                      <td>
+                        {appointment.hasVisited === true ? (
+                          <GoCheckCircleFill className="green" />
+                        ) : (
+                          <AiFillCloseCircle className="red" />
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <h1>No Appointments</h1>
+              )}
+            </tbody>
+          </table>
         </div>
       </section>
     </>
