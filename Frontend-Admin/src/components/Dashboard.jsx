50c6d9d6c7f1a1308307f1e50f5a746d557c42bf
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-06 00:16:01 +0530

    Dashboard banner first box created

--- Frontend-Admin/src/components/Dashboard.jsx
+++ Frontend-Admin/src/components/Dashboard.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const Dashboard = () => {
+  const { isAuthenticated, user } = useContext(Context);
+
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/appointment/getall",
+          { withCredentials: true }
+        );
+        setAppointments(data.appointments);
+      } catch (error) {
+        setAppointments({});
+        console.log("Some Error Occured While Fetching Appointments", error);
+      }
+    };
+    fetchAppointments();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="dashboard">
+        <div className="banner">
+          <div className="firstBox">
+            <img src="/doc.png" alt="Admin" />
+            <div className="content">
+              <div>
+                <p>Hello, </p>
+                <h5>{user && `${user.firstName} ${user.lastName}`}</h5>
+              </div>
+              <p>
+                The Life Care Administration panel allows admins to add new
+                administrators, register doctors, and manage patient
+                appointments. Admins can view, approve, or reject appointment
+                requests, ensuring smooth hospital operations and improved
+                efficiency.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default Dashboard;
