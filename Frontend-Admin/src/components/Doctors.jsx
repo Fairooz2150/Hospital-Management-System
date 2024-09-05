d9d143f0ebed912307adcf719863e86aef7bbf6a
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-05 17:21:31 +0530

    Doctors page designerd

--- Frontend-Admin/src/components/Doctors.jsx
+++ Frontend-Admin/src/components/Doctors.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useContext, useEffect } from "react";
+import { Context } from "../main";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+
+  useEffect(() => {
+    const fetchdoctrs = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/user/doctors",
+          { withCredentials: true }
+        );
+        setDoctors(data.doctors);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+    fetchdoctrs();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="page doctors">
+        <h1>Doctors</h1>
+        <div className="banner">{doctors && doctors.length > 0 ? (doctors.map((element, index)=>{
+          return(
+            <div className="card" key={index}>
+                  <img src={element.doctrAvatar && element.doctrAvatar.url} alt="Doctor Avatar" />
+            <h4>{`${element.firstName} ${element.lastName}`}</h4>
+            <div className="details">
+              <p>Email:<span>{element.email}</span></p>
+              <p>Phone:<span>{element.phone}</span></p>
+              <p>DoB:<span>{element.dob}</span></p>
+              <p>Department:<span>{element.doctrdptmnt}</span></p>
+              <p>Aadhar:<span>{element.aadhar}</span></p>
+              <p>Gender:<span>{element.gender}</span></p>
+
+            </div>
+            </div>
+          )
+        })) : <h1>No Registered Doctors Found!</h1>}</div>
+      </section>
+    </>
   );
-}
+};
 
 export default Doctors;
