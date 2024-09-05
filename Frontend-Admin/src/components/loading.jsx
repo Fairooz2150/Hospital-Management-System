c241ca36b593a6f9a970e91aacdbe85cc6e4df18
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-06 02:30:19 +0530

    Creates a loading component

---
+++ Frontend-Admin/src/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading-overlay">
+      <img src="/loading.gif" alt="Loading..." className="loading-gif" />
+    </div>
+  );
+};
+
+export default Loading;
